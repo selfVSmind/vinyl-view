44a5b506a0c2fcdecb1586304b148be475b2ee62
Jason Lambert <jason@jasonlambert.io>
2020-11-21 10:46:59 -0500

    updated the pulse audio header to show my contributions to the base code

--- cpp/audio_manager.cpp
+++ cpp/audio_manager.cpp
@@ -1,21 +1,12 @@
+/***
+ * Some of the code in this file is from this sample from the PulseAudio website
+ * https://freedesktop.org/software/pulseaudio/doxygen/parec-simple_8c-example.html
+ * 
+ * All changes from that original file have been done by Jason Lambert
+***/
+
 #include "audio_manager.h"
 
-/***
-  This file is part of PulseAudio.
-  PulseAudio is free software; you can redistribute it and/or modify
-  it under the terms of the GNU Lesser General Public License as published
-  by the Free Software Foundation; either version 2.1 of the License,
-  or (at your option) any later version.
-  PulseAudio is distributed in the hope that it will be useful, but
-  WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-  General Public License for more details.
-  You should have received a copy of the GNU Lesser General Public License
-  along with PulseAudio; if not, see <http://www.gnu.org/licenses/>.
-***/
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
